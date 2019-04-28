66a72f7d61bea083d0115679638201bef7ca9458
Kris <christttto@berkeley.edu>
2019-04-27 20:38:41 -0700

    create folder

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,4 +1,9 @@
 const fs = require("fs")
 const path = require("path")
 
+//these are asynchronous
 //Create folder
+fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+  if (err) throw err
+  console.log("Folder Created")
+})
