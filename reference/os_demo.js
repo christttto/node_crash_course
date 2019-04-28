90d4a1e789fd94829ab72d61b7512e32a88c55a1
Kris <christttto@berkeley.edu>
2019-04-27 20:51:41 -0700

    os

---
+++ reference/os_demo.js
@@ -0,0 +1,15 @@
+const os = require("os")
+
+//platform
+console.log(os.platform())
+
+console.log(os.arch())
+
+//cpu core info
+console.log(os.cpus())
+
+//free memory
+console.log(os.freemem())
+
+//totla memory
+console.log(os.totalmem())
