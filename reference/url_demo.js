9d86eb9c6bad1fc4d37bad359327590ea0c0c561
Kris <christttto@berkeley.edu>
2019-04-27 20:58:26 -0700

    url

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -8,3 +8,18 @@ console.log(myUrl.toString())
 
 //host (root domain)
 console.log(myUrl.host)
+
+//it does not get port number
+console.log(myUrl.hostname)
+
+//pathname
+console.log(myUrl.pathname)
+//serialized query
+console.log(myUrl.search)
+//params object
+console.log(myUrl.searchParams)
+//add param
+myUrl.searchParams.append("abc", "123")
+console.log(myUrl.searchParams)
+//loop through params
+myUrl.searchParams.forEach((value, name) => console.log(`${name}:${value}`))
