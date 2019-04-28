277f76961e5f8df94d9a0171b145ddd9cc00b983
Kris <christttto@berkeley.edu>
2019-04-27 20:40:50 -0700

    text file create

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -3,7 +3,17 @@ const path = require("path")
 
 //these are asynchronous
 //Create folder
-fs.mkdir(path.join(__dirname, "/test"), {}, err => {
-  if (err) throw err
-  console.log("Folder Created")
-})
+// fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+//   if (err) throw err
+//   console.log("Folder Created")
+// })
+
+//create and write to file
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "hello world!",
+  err => {
+    if (err) throw err
+    console.log("File written to")
+  }
+)
